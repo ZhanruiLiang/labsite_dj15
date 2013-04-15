2bbabe71b86c44413a4de4696eb59d557fdb486a
Ray <ray040123@gmail.com>
2013-04-16 06:48:40 +0800

    hide passwd

--- labsite_dj15/settings.py
+++ labsite_dj15/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'labsite',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': open('passwd').read().strip(),                  # Not used with sqlite3.
+        'PASSWORD': open(relpath('passwd')).read().strip(),                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
